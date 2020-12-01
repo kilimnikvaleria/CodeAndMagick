2d9365ada79318339def8359281b5337ccfa633f
Valeria <kilimnikvaleria@gmail.com>
2020-12-01 21:35:01 +0300

    t3 | revisions for cycle

--- js/setup.js
+++ js/setup.js
@@ -2,17 +2,17 @@
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
-//функция генерации случайных данных для похожих волшебников
+//функция генерации случайных данных для похожих волшебников с уникальными именем и фамилией
 
 var getWizards = function () {
     var wizards = [];
 
     var dataWizards = {
-        count: 4,
-        names: [ "Иван", "Хуан Себастьян", "Мария", "Кристоф", "Виктор", "Юлия", "Люпита", "Вашингтон" ],
-        surnames: [ "да Марья", "Верон", "Мирабелла", "Вальц", "Онопко", "Топольницкая", "Нионго", "Ирвинг" ],
-        coatColors: [ "rgb(101, 137, 164)", "rgb(241, 43, 107)", "rgb(146, 100, 161)", "rgb(56, 159, 117)", "rgb(215, 210, 55)", "rgb(0, 0, 0)" ],
-        eyesColors: [ "black", "red", "blue", "yellow", "green" ],
+        count: 4, // должно быть всегда меньше или равно чем количество имен и фамилий в сумме
+        names: ["Иван", "Хуан Себастьян", "Мария", "Кристоф", "Виктор", "Юлия", "Люпита", "Вашингтон"],
+        surnames: ["да Марья", "Верон", "Мирабелла", "Вальц", "Онопко", "Топольницкая", "Нионго", "Ирвинг"],
+        coatColors: ["rgb(101, 137, 164)", "rgb(241, 43, 107)", "rgb(146, 100, 161)", "rgb(56, 159, 117)", "rgb(215, 210, 55)", "rgb(0, 0, 0)"],
+        eyesColors: ["black", "red", "blue", "yellow", "green"],
         getRandomElement: function (array) {
             var randomIndex = Math.floor(Math.random() * array.length);
             return array[randomIndex];
@@ -27,17 +27,26 @@ var getWizards = function () {
         }
 
     };
-    wizards.push(dataWizards.getRandomData());
-    while (wizards.length<dataWizards.count) {
+    // wizards.push(dataWizards.getRandomData());//необходимо для первого варианта
+    while (wizards.length < dataWizards.count) {
         var wizardsElement = dataWizards.getRandomData();
-        if (wizardsElement.name === wizards[wizards.length-1].name) {
-            wizardsElement = dataWizards.getRandomData();
+        // начало первого варианта
+        // var isContains = false
+        // for(var wizard in wizards) {
+        //     if(wizard.name === wizardsElement.name) {
+        //         isContains = true;
+        //     }
+        // }
+        // if(!isContains) {
+        //     wizards.push(wizardsElement)
+        // }
+        //конец первого варианта
+        //начало второго варианта
+        if (wizards.every(function (wizard)  { return wizard.name !== wizardsElement.name })) {
+            wizards.push(wizardsElement)
         }
-        wizards.push(dataWizards.getRandomData());
+        //конец второго варианта
     }
-    /*for (var i = 0; i < wizards.length; i++) {
-        console.log(wizards[i]);
-    }*/
     return wizards;
 };
 
@@ -64,6 +73,9 @@ var renderWizards = function () {
     for (var i = 0; i < similarWizards.length; i++) {
         fragment.appendChild(createWizard(similarWizards[i]));
     }
+    // similarWizards.forEach(function (wizard) { fragment.appendChild(createWizard(wizard))}) // алтернатива для предыдущего цикла
+    // similarWizards.forEach((wizard) => fragment.appendChild(createWizard(wizard))) // алтернатива для предыдущего цикла
+
     similarListElement.appendChild(fragment);
     document.querySelector('.setup-similar').classList.remove('hidden');
 }();
